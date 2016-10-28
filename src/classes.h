7a1ce7970385bdf439e136e1aafa17be0a0104cf
animesh2049 <animesh2049@gmail.com>
2016-10-29 01:51:36 +0530

    Added more classes

--- src/classes.h
+++ src/classes.h
@@ -10,10 +10,10 @@ class Field_Decl;
 class Var_Decl;
 class Method_Decl;
 class Identifier;
+class Arr_Identifier;
 class Statement;
 class Expression;
 class Data_Type;
-class Arr_Identifier;
 class Assign_Op;
 class Callout_Arg;
 class Literal;
@@ -21,12 +21,43 @@ class Location;
 class Method_Call;
 class Block;
 
+enum class Bin_Op : char {
+  op_plus;
+  op_minus;
+  op_multiply;
+  op_divide;
+  op_modulo;
+  op_greater_than;
+  op_less_than;
+  op_less_equal;
+  op_greater_equal;
+  op_logical_or;
+  op_logical_and;
+  op_equal_equal;
+  op_not_equal;
+};
+
+enum class Data_Type : char {
+  int_type;
+  void_type;
+  bool_type;
+};
+
+enum class Assign_Op : char {
+  op_plus_equal;
+  op_minus_equal;
+  op_equal;
+};
+
+enum class Unary_Op : char {
+  op_minus;
+  op_not;
+};
 
 class Program {
   std::string id;
   std::vector<Field_Decl *> *field_decl_list;
   std::vector<Method_Decl *> *method_decl_list;
-  std::vector<Statement *> *statement_decl_list; // Check ?
 public:
   Program(std::string id, std::vector<Field_Decl *> *field_decl_list,
     std::vector<Method_Decl *> *method_decl_list,
@@ -78,17 +109,65 @@ public:
   ~Field_Decl(){}
 }
 
+class Identifier {
+  std::string id;
+public:
+  Identifier(){}
+  std::string get_id(){
+    return this->id;
+  }
+  ~Identifier(){}
+};
+
+class Arr_Identifier : public Identifier {
+  int size;
+public:
+  Arr_Identifier(std::string id, int size){
+    this->id = id;
+    this->size = size;
+  }
+  int get_size(){
+    return this->size;
+  }
+  ~Arr_Identifier(){}
+};
+
+class Var_Decl {
+  Data_Type type;
+  std::vector<Identifier *> *identifier;
+  std::vector<Arr_Identifier *>    *arr_identifier;
+public:
+  Var_Decl(std::vector<Identifier *> *identifier,
+    std::vector<Arr_Identifier *> *arr_identifier, Data_Type type) {
+      this->identifier = identifier;
+      this->arr_identifier = arr_identifier;
+      this->type = type;
+    }
+  std::vector<Normal_Identifier *> *get_id_list(){
+    return this->identifier;
+  }
+  std::vector<Arr_Identifier *> *get_arr_id_list(){
+    return this->arr_identifier;
+  }
+  Data_Type get_type(){
+    return this->type;
+  }
+};
+
 class Method_Decl {
   std::string id;
   Data_Type return_type;
   std::vector<Identifier *> *arguments;
+  std::vector<Arr_Identifier *> *arr_arguments;
   Block my_block;
 public:
   Method_Decl(std::string id, Data_Type return_type,
-    std::vector<Identifier *> *arguments, Block my_block){
+    std::vector<Identifier *> *arguments,
+    std::vector<Arr_Identifier *> *arr_arguments, Block my_block){
       this->id = id;
       this->data_type = data_type;
       this->arguments = arguments;
+      this->arr_arguments = arr_arguments;
       this->my_block = my_block;
     }
   std::string get_id(){
@@ -97,16 +176,14 @@ public:
   Data_Type get_return_type(){
     return this->return_type;
   }
-  std::vector<Identifier *> get_arguments(){
+  std::vector<Identifier *> *get_arguments(){
     return this->arguments;
+  }
+  std::vector<Arr_Identifier *> *get_arr_arguments(){
+    return this->arr_arguments;
   }
   Block get_block(){
     return this->my_block;
   }
   ~Method_Decl(){}
 };
-
-class Var_Decl {
-  Data_Type data_type;
-
-}
